286a29d02c561b186d9b8c3333823e3dc5c8e65e
Maksim <maks.mexx11101982@gmail.com>
2022-06-28 14:11:46 +0500

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Shop Brand</title>
+				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+				<meta charSet='utf-8' />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
